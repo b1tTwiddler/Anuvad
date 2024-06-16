4a7afe152d5e499eb6623a7d868da9e5e3e8bd0e
b1tTwiddler <bhattacharyasoumabrata@gmail.com>
2024-06-16 18:06:21 +0530

    added angular framework

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: [
-		"./*.html",
-		"./src/**/*.{js,ts,html}",
-		"./node_modules/flowbite/**/*.js",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("flowbite/plugin")],
+module.exports = {
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
 };
